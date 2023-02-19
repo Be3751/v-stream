4270d333a1ebc661c5fc3198e89c769ac848490c
薗部匡矢 <msonobe@msonobe.local>
2023-02-19 13:54:27 +0900

    implement server running

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
 
+	"github.com/Be3751/v-stream/pkg/pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	port := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+	srv := grpc.NewServer()
+
+	pb.RegisterVideoStreamServer(srv, NewMyServer())
+
+	go func() {
+		log.Printf("start gRPC server port: %d", port)
+		srv.Serve(listener)
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	srv.GracefulStop()
+}
+
+type myServer struct {
+	pb.UnimplementedVideoStreamServer
+}
+
+func NewMyServer() *myServer {
+	return &myServer{}
+}
+
+func (s *myServer) ReceiveVideo(*pb.SendRequest, pb.VideoStream_ReceiveVideoServer) error {
+	return nil
 }
--- pkg/pb/stream.pb.go
+++ pkg/pb/stream.pb.go
@@ -2,9 +2,9 @@
 // versions:
 // 	protoc-gen-go v1.28.1
 // 	protoc        v3.21.12
-// source: api/proto/stream.proto
+// source: stream.proto
 
-package proto
+package pb
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
@@ -31,7 +31,7 @@ type SendRequest struct {
 func (x *SendRequest) Reset() {
 	*x = SendRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_api_proto_stream_proto_msgTypes[0]
+		mi := &file_stream_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -44,7 +44,7 @@ func (x *SendRequest) String() string {
 func (*SendRequest) ProtoMessage() {}
 
 func (x *SendRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_api_proto_stream_proto_msgTypes[0]
+	mi := &file_stream_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -57,7 +57,7 @@ func (x *SendRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use SendRequest.ProtoReflect.Descriptor instead.
 func (*SendRequest) Descriptor() ([]byte, []int) {
-	return file_api_proto_stream_proto_rawDescGZIP(), []int{0}
+	return file_stream_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *SendRequest) GetVideoId() string {
@@ -79,7 +79,7 @@ type SendResponse struct {
 func (x *SendResponse) Reset() {
 	*x = SendResponse{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_api_proto_stream_proto_msgTypes[1]
+		mi := &file_stream_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -92,7 +92,7 @@ func (x *SendResponse) String() string {
 func (*SendResponse) ProtoMessage() {}
 
 func (x *SendResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_api_proto_stream_proto_msgTypes[1]
+	mi := &file_stream_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -105,7 +105,7 @@ func (x *SendResponse) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use SendResponse.ProtoReflect.Descriptor instead.
 func (*SendResponse) Descriptor() ([]byte, []int) {
-	return file_api_proto_stream_proto_rawDescGZIP(), []int{1}
+	return file_stream_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *SendResponse) GetName() string {
@@ -122,46 +122,47 @@ func (x *SendResponse) GetVideo() []byte {
 	return nil
 }
 
-var File_api_proto_stream_proto protoreflect.FileDescriptor
+var File_stream_proto protoreflect.FileDescriptor
 
-var file_api_proto_stream_proto_rawDesc = []byte{
-	0x0a, 0x16, 0x61, 0x70, 0x69, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x73, 0x74, 0x72, 0x65,
-	0x61, 0x6d, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x27, 0x0a, 0x0b, 0x53, 0x65, 0x6e, 0x64,
+var file_stream_proto_rawDesc = []byte{
+	0x0a, 0x0c, 0x73, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x08,
+	0x76, 0x5f, 0x73, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x22, 0x27, 0x0a, 0x0b, 0x53, 0x65, 0x6e, 0x64,
 	0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x18, 0x0a, 0x07, 0x76, 0x69, 0x64, 0x65, 0x6f,
 	0x49, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x76, 0x69, 0x64, 0x65, 0x6f, 0x49,
 	0x64, 0x22, 0x38, 0x0a, 0x0c, 0x53, 0x65, 0x6e, 0x64, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73,
 	0x65, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52,
 	0x04, 0x6e, 0x61, 0x6d, 0x65, 0x12, 0x14, 0x0a, 0x05, 0x76, 0x69, 0x64, 0x65, 0x6f, 0x18, 0x02,
-	0x20, 0x01, 0x28, 0x0c, 0x52, 0x05, 0x76, 0x69, 0x64, 0x65, 0x6f, 0x32, 0x36, 0x0a, 0x06, 0x53,
-	0x74, 0x72, 0x65, 0x61, 0x6d, 0x12, 0x2c, 0x0a, 0x09, 0x53, 0x65, 0x6e, 0x64, 0x56, 0x69, 0x64,
-	0x65, 0x6f, 0x12, 0x0c, 0x2e, 0x53, 0x65, 0x6e, 0x64, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74,
-	0x1a, 0x0d, 0x2e, 0x53, 0x65, 0x6e, 0x64, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22,
-	0x00, 0x30, 0x01, 0x42, 0x26, 0x5a, 0x24, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f,
-	0x6d, 0x2f, 0x42, 0x65, 0x33, 0x37, 0x35, 0x31, 0x2f, 0x76, 0x2d, 0x73, 0x74, 0x72, 0x65, 0x61,
-	0x6d, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x06, 0x70, 0x72, 0x6f,
-	0x74, 0x6f, 0x33,
+	0x20, 0x01, 0x28, 0x0c, 0x52, 0x05, 0x76, 0x69, 0x64, 0x65, 0x6f, 0x32, 0x50, 0x0a, 0x0b, 0x56,
+	0x69, 0x64, 0x65, 0x6f, 0x53, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x12, 0x41, 0x0a, 0x0c, 0x52, 0x65,
+	0x63, 0x65, 0x69, 0x76, 0x65, 0x56, 0x69, 0x64, 0x65, 0x6f, 0x12, 0x15, 0x2e, 0x76, 0x5f, 0x73,
+	0x74, 0x72, 0x65, 0x61, 0x6d, 0x2e, 0x53, 0x65, 0x6e, 0x64, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
+	0x74, 0x1a, 0x16, 0x2e, 0x76, 0x5f, 0x73, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x2e, 0x53, 0x65, 0x6e,
+	0x64, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x30, 0x01, 0x42, 0x23, 0x5a,
+	0x21, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x42, 0x65, 0x33, 0x37,
+	0x35, 0x31, 0x2f, 0x76, 0x2d, 0x73, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x2f, 0x70, 0x6b, 0x67, 0x2f,
+	0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_api_proto_stream_proto_rawDescOnce sync.Once
-	file_api_proto_stream_proto_rawDescData = file_api_proto_stream_proto_rawDesc
+	file_stream_proto_rawDescOnce sync.Once
+	file_stream_proto_rawDescData = file_stream_proto_rawDesc
 )
 
-func file_api_proto_stream_proto_rawDescGZIP() []byte {
-	file_api_proto_stream_proto_rawDescOnce.Do(func() {
-		file_api_proto_stream_proto_rawDescData = protoimpl.X.CompressGZIP(file_api_proto_stream_proto_rawDescData)
+func file_stream_proto_rawDescGZIP() []byte {
+	file_stream_proto_rawDescOnce.Do(func() {
+		file_stream_proto_rawDescData = protoimpl.X.CompressGZIP(file_stream_proto_rawDescData)
 	})
-	return file_api_proto_stream_proto_rawDescData
+	return file_stream_proto_rawDescData
 }
 
-var file_api_proto_stream_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_api_proto_stream_proto_goTypes = []interface{}{
-	(*SendRequest)(nil),  // 0: SendRequest
-	(*SendResponse)(nil), // 1: SendResponse
+var file_stream_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_stream_proto_goTypes = []interface{}{
+	(*SendRequest)(nil),  // 0: v_stream.SendRequest
+	(*SendResponse)(nil), // 1: v_stream.SendResponse
 }
-var file_api_proto_stream_proto_depIdxs = []int32{
-	0, // 0: Stream.SendVideo:input_type -> SendRequest
-	1, // 1: Stream.SendVideo:output_type -> SendResponse
+var file_stream_proto_depIdxs = []int32{
+	0, // 0: v_stream.VideoStream.ReceiveVideo:input_type -> v_stream.SendRequest
+	1, // 1: v_stream.VideoStream.ReceiveVideo:output_type -> v_stream.SendResponse
 	1, // [1:2] is the sub-list for method output_type
 	0, // [0:1] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -169,13 +170,13 @@ var file_api_proto_stream_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_api_proto_stream_proto_init() }
-func file_api_proto_stream_proto_init() {
-	if File_api_proto_stream_proto != nil {
+func init() { file_stream_proto_init() }
+func file_stream_proto_init() {
+	if File_stream_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_api_proto_stream_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_stream_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*SendRequest); i {
 			case 0:
 				return &v.state
@@ -187,7 +188,7 @@ func file_api_proto_stream_proto_init() {
 				return nil
 			}
 		}
-		file_api_proto_stream_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_stream_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*SendResponse); i {
 			case 0:
 				return &v.state
@@ -204,18 +205,18 @@ func file_api_proto_stream_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_api_proto_stream_proto_rawDesc,
+			RawDescriptor: file_stream_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
-		GoTypes:           file_api_proto_stream_proto_goTypes,
-		DependencyIndexes: file_api_proto_stream_proto_depIdxs,
-		MessageInfos:      file_api_proto_stream_proto_msgTypes,
+		GoTypes:           file_stream_proto_goTypes,
+		DependencyIndexes: file_stream_proto_depIdxs,
+		MessageInfos:      file_stream_proto_msgTypes,
 	}.Build()
-	File_api_proto_stream_proto = out.File
-	file_api_proto_stream_proto_rawDesc = nil
-	file_api_proto_stream_proto_goTypes = nil
-	file_api_proto_stream_proto_depIdxs = nil
+	File_stream_proto = out.File
+	file_stream_proto_rawDesc = nil
+	file_stream_proto_goTypes = nil
+	file_stream_proto_depIdxs = nil
 }
